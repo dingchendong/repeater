e09b88d9516c9e8f463227cba8d3bc33467b04e7
dingchendong <chendong.ding@d3j.io>
2020-01-09 10:52:23 +0800

    update:repeater

--- example/index.js
+++ example/index.js
@@ -1,6 +1,31 @@
+const http = require('http');
+
+let repeater = require("../lib/");
+
 let params = {
 	urls: ["http://127.0.0.1:8091", "http://127.0.0.1:8092", "http://127.0.0.1:8093"],
 	port: 8081
 };
 
-require("../lib/")(params);
+var httpServer = new http.Server(params.port, [
+	(req) => {
+		req.session = {}
+	}, {
+		"*": repeater(params.urls)
+	}
+]);
+
+
+setInterval(() => {
+	console.warn("server is running", new Date());
+}, 1000 * 5);
+
+
+httpServer.enableCrossOrigin();
+
+let version = process.version.split("-")[0].split('.')[1];
+if (Number(version) < 29) {
+	httpServer.run(() => {});
+} else {
+	httpServer.start();
+}
